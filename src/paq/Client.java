9eb8af750e4848c6def53186ddfc69893060f88c
ojcastaneda <oj.castaneda@uniandes.edu.co>
2020-10-06 14:44:45 -0500

    Sistema de envio de archivos hecho

--- src/paq/Client.java
+++ src/paq/Client.java
@@ -9,51 +9,71 @@ public class Client {
 
 	private DataOutputStream os;
 	private DataInputStream is;
+	private ObjectInputStream br;
 	private MessageDigest md;
 	private Socket socket;
 
-	public Client() throws Exception{
-			iniciarConexion();
-			os.close();
-			is.close();
-			socket.close();
+	public Client() throws Exception {
+		iniciarConexion();
+		os.close();
+		is.close();
+		socket.close();
 	}
 
-	public void iniciarConexion() throws Exception{
+	public void iniciarConexion() throws Exception {
 		socket = new Socket(Server.IP, Server.PUERTO);
 		os = new DataOutputStream(socket.getOutputStream());
 		is = new DataInputStream(socket.getInputStream());
+		br = new ObjectInputStream(socket.getInputStream());
 		md = MessageDigest.getInstance(Server.HASH);
 		System.out.println("Conectado");
 		os.writeUTF("Preparado");
 		System.out.println("Preparado para recibir la respuesta del servidor");
-		String documento = is.readUTF();
+		pedirArchivo();
+	}
+
+	public void pedirShrek() throws Exception {
+		System.out.println("Enviando petición de Shrek");
+		os.writeUTF("250mb");
+		os.flush();
+		pedirArchivo();
+	}
+
+	public void pedirInterstellar() throws Exception {
+		System.out.println("Enviando petición de Interstellar");
+		os.writeUTF("100mb");
+		os.flush();
+		pedirArchivo();
+	}
+
+	public String pedirArchivo() throws Exception {
+		String documento = (String) br.readObject();
 		int size = is.readInt();
 		byte[] hash = new byte[size];
-		for(int i=0;i<size;i++) {
+		for (int i = 0; i < size; i++) {
 			hash[i] = is.readByte();
 		}
-		if(verificacionHashString(documento, hash)) {
+		if (verificacionHashString(documento, hash)) {
 			os.writeUTF("Recibido");
 			System.out.println("Mensaje verificado");
 		}
+		return documento;
 	}
-	
+
 	public boolean verificacionHashString(String string, byte[] hash) throws Exception {
-		if(Arrays.equals(md.digest(string.getBytes("UTF-8")), hash)) {
+		if (Arrays.equals(md.digest(string.getBytes("UTF-8")), hash)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public static void main(String args[]) {
 		try {
 			new Client();
-
-		}
-		catch(Exception e) {
+			
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			e.printStackTrace();	
+			e.printStackTrace();
 		}
 	}
 }
--- src/paq/Server.java
+++ src/paq/Server.java
@@ -1,27 +1,28 @@
 package paq;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
+import java.security.MessageDigest;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Server {
 
+	public final static String PRUEBA = "./Files/prueba.txt";
+	public final static String SHREK = "./Files/Shrek1-2Script.txt";
+	public final static String INTERSTELLAR = "./Files/Interstellar-script.txt";
 	public final static String IP = "127.0.0.1";
 	public final static String HASH = "SHA-256";
 	public final static int PUERTO = 5000;
 
-	private BufferedReader consola;
+	private static String interstellar;
+	private static BufferedWriter bw;
+	private static String prueba;
+	private static String shrek;
+
+	static File log = new File("./Files/log.txt");
 	private ExecutorService pool;
 	private ServerSocket server;
-	private static BufferedWriter bw;
-	private String documento;
 	private Socket socket;
 
 	public Server() throws Exception {
@@ -30,51 +31,61 @@ public class Server {
 		while (true) {
 			imprimir("Esperando conexion ...");
 			socket = server.accept();
-			pool.execute(new ThreadServidor(socket, documento));
+			pool.execute(new ThreadServidor(socket));
 			imprimir("Cliente aceptado");
 			break;
 		}
 		pool.shutdown();
-		pool.awaitTermination(5, TimeUnit.SECONDS);
-		consola.close();
-		bw.close();
-		server.close();
 	}
 
-	public void inicializarServidor() throws Exception{
-		File log = new File("./Files/log.txt");
-		if(!log.exists()) {
+	public void inicializarServidor() throws Exception {
+		if (!log.exists()) {
 			log.createNewFile();
 		}
 		bw = new BufferedWriter(new FileWriter(log));
 		server = new ServerSocket(PUERTO);
-		consola = new BufferedReader(new InputStreamReader(System.in));
-		leerArchivoTxt();
+		prueba = leerArchivoTxt(PRUEBA);
+		shrek = leerArchivoTxt(SHREK);
+		interstellar = leerArchivoTxt(INTERSTELLAR);
 		pool = Executors.newFixedThreadPool(25);
 	}
-	
-	public void leerArchivoTxt() throws Exception {
-		BufferedReader br = new BufferedReader(new FileReader(new File("./Files/prueba.txt")));
+
+	public String leerArchivoTxt(String direccion) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(new File(direccion)));
 		String next = br.readLine();
 		StringBuilder concatenador = new StringBuilder(next);
 		int i = 0;
 		while (next != null) {
-			if(i!=0) {
+			if (i != 0) {
 				concatenador.append("\n").append(next);
 			}
 			next = br.readLine();
 			i++;
 		}
 		br.close();
-		documento = concatenador.toString();
+		return concatenador.toString();
 	}
-	
+
+	public static String darPrueba() {
+		return prueba;
+	}
+
+	public static String darShrek() {
+		return shrek;
+	}
+
+	public static String darInterstellar() {
+		return interstellar;
+	}
+
 	public static synchronized void imprimir(String string) {
 		System.out.println(string);
 	}
-	
-	public static synchronized void imprimirLog(String string) throws Exception{
-		bw.write(string +"\n");
+
+	public static synchronized void imprimirLog(String string) throws Exception {
+		bw.write(string + "\n");
+		bw.close();
+		bw = new BufferedWriter(new FileWriter(log, true));
 	}
 
 	public static void main(String args[]) {
@@ -85,4 +96,122 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
+
+	class ThreadServidor implements Runnable {
+
+		private Socket socket;
+		private DataOutputStream os;
+		private DataInputStream is;
+		private ObjectOutputStream bw;
+		private MessageDigest md;
+
+		public ThreadServidor(Socket socket) throws Exception {
+			this.socket = socket;
+			os = new DataOutputStream(socket.getOutputStream());
+			is = new DataInputStream(socket.getInputStream());
+			bw = new ObjectOutputStream(socket.getOutputStream());
+			md = MessageDigest.getInstance(Server.HASH);
+		}
+
+		public void enviarHashString(String string) throws Exception {
+			byte[] info = md.digest(string.getBytes("UTF-8"));
+			os.writeInt(info.length);
+			os.flush();
+			os.write(info);
+			os.flush();
+		}
+
+		public void enviarString(String string) throws Exception {
+			bw.writeObject(string);
+			bw.flush();
+		}
+
+		public void enviarArchivo() throws Exception {
+			String peticion = is.readUTF();
+			if (peticion.equals("250mb")) {
+				enviarShrek();
+			} else if (peticion.equals("100mb")) {
+				enviarInterstellar();
+			}
+		}
+
+		public void iniciarConexion() throws Exception {
+			StringBuilder concatenador = new StringBuilder("Tiempo de envio del documento de prueba: ");
+			if (is.readUTF().equals("Preparado")) {
+				Server.imprimir("Enviando documento");
+				long tiempo = System.currentTimeMillis();
+				enviarString(Server.darPrueba());
+				enviarHashString(Server.darPrueba());
+				if (!is.readUTF().equals("Recibido")) {
+					tiempo = System.currentTimeMillis() - tiempo;
+					Server.imprimirLog(
+							concatenador.append(tiempo).append("ms. El estado del envio fue incompleto").toString());
+					throw new Exception();
+				}
+				tiempo = System.currentTimeMillis() - tiempo;
+				Server.imprimirLog(
+						concatenador.append(tiempo).append("ms. El estado del envio fue completo").toString());
+				Server.imprimir("Comprobante recibido");
+			} else {
+				throw new Exception();
+			}
+		}
+
+		public void enviarInterstellar() throws Exception {
+			Server.imprimir("Enviando documento");
+			StringBuilder concatenador = new StringBuilder("Tiempo de envio del documento de 100mb: ");
+			long tiempo = System.currentTimeMillis();
+			enviarString(Server.darInterstellar());
+			enviarHashString(Server.darInterstellar());
+			if (!is.readUTF().equals("Recibido")) {
+				tiempo = System.currentTimeMillis() - tiempo;
+				Server.imprimirLog(
+						concatenador.append(tiempo).append("ms. El estado del envio fue incompleto").toString());
+				throw new Exception();
+			}
+			tiempo = System.currentTimeMillis() - tiempo;
+			Server.imprimirLog(concatenador.append(tiempo).append("ms. El estado del envio fue completo").toString());
+			Server.imprimir("Comprobante recibido");
+		}
+
+		public void enviarShrek() throws Exception {
+			Server.imprimir("Enviando documento");
+			StringBuilder concatenador = new StringBuilder("Tiempo de envio del documento de 250mb: ");
+			long tiempo = System.currentTimeMillis();
+			enviarString(Server.darShrek());
+			enviarHashString(Server.darShrek());
+			if (!is.readUTF().equals("Recibido")) {
+				tiempo = System.currentTimeMillis() - tiempo;
+				Server.imprimirLog(
+						concatenador.append(tiempo).append("ms. El estado del envio fue incompleto").toString());
+				throw new Exception();
+			}
+			tiempo = System.currentTimeMillis() - tiempo;
+			Server.imprimirLog(concatenador.append(tiempo).append("ms. El estado del envio fue completo").toString());
+			Server.imprimir("Comprobante recibido");
+		}
+
+		public void run() {
+			try {
+				iniciarConexion();
+				enviarArchivo();
+				os.close();
+				is.close();
+				socket.close();
+
+			} catch (Exception e) {
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					System.out.println(e1.getMessage());
+					e1.printStackTrace();
+				}
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 }
